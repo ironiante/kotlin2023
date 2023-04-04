70ca20bfc49e82c36d76f7b631c28cb02d6bb906
jirma <jirman.rodriguez@uptc.edu.co>
2023-04-04 18:29:26 -0500

    trabajando con variables

---
+++ src/main/kotlin/start/Trabajando con variables en Kotlin.kt
@@ -0,0 +1,22 @@
+package start
+fun  main (){
+val numero1=30
+val numero2=45
+
+println("sumar")
+println(numero1+numero2)
+
+println("restar")
+println(numero1 - numero2)
+
+println("multiplicar")
+println(numero1 * numero2)
+
+println("division")
+println(numero2 / numero1)
+
+println("modulo")
+println(numero2 % numero2)
+
+}
+
