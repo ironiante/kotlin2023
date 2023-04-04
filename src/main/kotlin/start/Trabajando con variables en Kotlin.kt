2571c24f5e36f3a5071f8b0941c03083a7e31744
jirma <jirman.rodriguez@uptc.edu.co>
2023-04-04 18:58:14 -0500

    trabajando con variables concatenar

--- src/main/kotlin/start/Trabajando con variables en Kotlin.kt
+++ src/main/kotlin/start/Trabajando con variables en Kotlin.kt
@@ -2,21 +2,29 @@ package start
 fun  main (){
 val numero1=30
 val numero2=45
+var age=42
 
-println("sumar")
-println(numero1+numero2)
+//println("sumar")
+//println(numero1+numero2)
 
-println("restar")
-println(numero1 - numero2)
+//println("restar")
+//println(numero1 - numero2)
 
-println("multiplicar")
-println(numero1 * numero2)
+//println("multiplicar")
+//println(numero1 * numero2)
 
-println("division")
-println(numero2 / numero1)
+//println("division")
+//println(numero2 / numero1)
 
-println("modulo")
-println(numero2 % numero2)
+//println("modulo")
+//println(numero2 % numero2)
+
+// concatenar
+val stringExample2="rodriguez"
+var stringConcatenada:String="hola"
+stringConcatenada= " me llamo jirman $stringExample2 $age "
+println(stringConcatenada)
+
 
 }
 
